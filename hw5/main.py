9bfe3e3bc785ce2f019604c22e782fae8bf4c99d
OleksandrZahar0vi4 <zaharovich007@gmail.com>
2024-02-27 18:59:17 +0200

    second_commit

--- hw5/main.py
+++ hw5/main.py
@@ -1,9 +1,14 @@
+# import aiofiles
+# import aiopath
 import asyncio
 from datetime import datetime, timedelta
 import httpx
+import logging
 import platform
 import sys
 import tabulate
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(funcName)5s - %(message)s")
 
 class HttpError(Exception):
     pass
@@ -22,9 +27,8 @@ async def request(url: str):
 
 
 async def exchange_currency(result: dict, currensy: str):
-    currensy = await input("Enter currensy: ")
+    # currensy = await input("Enter currensy: ")
     async for key, valume in result.items():
-        
             if isinstance(valume, list):
                 for val in valume:
                     if isinstance(val, dict):
@@ -34,28 +38,35 @@ async def exchange_currency(result: dict, currensy: str):
                                 for kk, vv in valu.items():
                                     valu[kk]=str(vv).split()
                                     head = ["baseCurrency", "currency", "sale", "purchase"]
-                                    data = await tabulate(valu, headers=head, tablefmt="grid")
+                                    data = tabulate(valu, headers=head, tablefmt="grid")
+                                    
                                     print(data)
-            
+                
                                 
         
         
 
 async def main(index_day):
-    date_new = datetime.now() - timedelta(days=int(index_day))
-    date_ques = date_new.strftime("%d.%m.%Y")
-    print(date_ques)
-    try:
-        response = await request(f'https://api.privatbank.ua/p24api/exchange_rates?date={date_ques}')
-        return response
-    except HttpError as err:
-        print(err)
-        return None
-
+    logging.debug(f"result: ")
+    if int(index_day) <= 10:
+        date_new = datetime.now() - timedelta(days=int(index_day))
+        date_ques = date_new.strftime("%d.%m.%Y")
+        print(date_ques)
+        try:
+            response = await request(f'https://api.privatbank.ua/p24api/exchange_rates?date={date_ques}')
+            return response
+        except HttpError as err:
+            print(err)
+            return None
+    else:
+        return "Enter index day beetwen (1, 10)"
 
 if __name__ == '__main__':
+    currensy = input("Enter currensy: ")
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     r = asyncio.run(main(sys.argv[1]))
-    # rr = exchange_currency(result=r)
+    
     print(r)
+    exchange_currency(r, currensy)
+    
--- mn.py
+++ mn.py
@@ -1,7 +1,11 @@
 from tabulate import tabulate
 import asyncio
 import sys
-current = input("Enter current: ")
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(funcName)5s - %(message)s")
+
+# current = input("Enter current: ")
 result = {'date': '19.02.2024', 'bank': 'PB', 'baseCurrency': 980, 'baseCurrencyLit': 'UAH', 'exchangeRate': [{'baseCurrency': 'UAH', 'currency': 'AUD', 'saleRateNB': 24.7615, 'purchaseRateNB': 24.7615}, {'baseCurrency': 'UAH', 'currency': 'AZN', 'saleRateNB': 22.3496, 'purchaseRateNB': 22.3496}, {'baseCurrency': 'UAH', 'currency': 'BYN', 'saleRateNB': 13.8019, 'purchaseRateNB': 13.8019}, {'baseCurrency': 'UAH', 'currency': 'CAD', 'saleRateNB': 28.1659, 'purchaseRateNB': 28.1659}, {'baseCurrency': 'UAH', 'currency': 'CHF', 'saleRateNB': 43.0911, 'purchaseRateNB': 43.0911, 'saleRate': 43.73, 'purchaseRate': 43.03}, {'baseCurrency': 'UAH', 'currency': 'CNY', 'saleRateNB': 5.2786, 'purchaseRateNB': 5.2786}, {'baseCurrency': 'UAH', 'currency': 'CZK', 'saleRateNB': 1.6063, 'purchaseRateNB': 1.6063, 'saleRate': 1.63, 'purchaseRate': 1.6}, {'baseCurrency': 'UAH', 'currency': 'DKK', 'saleRateNB': 5.4853, 'purchaseRateNB': 5.4853}, {'baseCurrency': 'UAH', 'currency': 'EUR', 'saleRateNB': 40.8919, 'purchaseRateNB': 40.8919, 'saleRate': 41.65, 'purchaseRate': 40.65}, {'baseCurrency': 'UAH', 'currency': 'GBP', 'saleRateNB': 47.7838, 'purchaseRateNB': 47.7838, 'saleRate': 48.58, 'purchaseRate': 47.8}, {'baseCurrency': 'UAH', 'currency': 'GEL', 'saleRateNB': 14.0238, 
 'purchaseRateNB': 14.0238}, {'baseCurrency': 'UAH', 'currency': 'HUF', 'saleRateNB': 0.105019, 'purchaseRateNB': 0.105019}, {'baseCurrency': 'UAH', 'currency': 'ILS', 'saleRateNB': 10.5264, 'purchaseRateNB': 10.5264}, {'baseCurrency': 'UAH', 'currency': 'JPY', 'saleRateNB': 0.25266, 'purchaseRateNB': 0.25266}, {'baseCurrency': 'UAH', 'currency': 'KZT', 'saleRateNB': 0.084435, 'purchaseRateNB': 0.084435}, {'baseCurrency': 'UAH', 'currency': 'MDL', 'saleRateNB': 2.1234, 'purchaseRateNB': 2.1234}, {'baseCurrency': 'UAH', 'currency': 'NOK', 'saleRateNB': 3.6016, 'purchaseRateNB': 3.6016}, {'baseCurrency': 'UAH', 'currency': 'PLN', 'saleRateNB': 9.4223, 'purchaseRateNB': 9.4223, 'saleRate': 9.78, 'purchaseRate': 9.38}, {'baseCurrency': 'UAH', 'currency': 'SEK', 'saleRateNB': 3.6286, 'purchaseRateNB': 3.6286}, {'baseCurrency': 'UAH', 'currency': 'SGD', 'saleRateNB': 28.2025, 'purchaseRateNB': 28.2025}, {'baseCurrency': 'UAH', 'currency': 'TMT', 'saleRateNB': 10.7322, 'purchaseRateNB': 10.7322}, {'baseCurrency': 'UAH', 'currency': 'TRY', 'saleRateNB': 1.2318, 'purchaseRateNB': 1.2318}, {'baseCurrency': 'UAH', 'currency': 'UAH', 'saleRateNB': 1.0, 'purchaseRateNB': 1.0}, {'baseCurrency': 'UAH', 'currency': 'USD', 'saleRateNB': 37.9719, 'purchaseRateNB': 37.9719, 'saleRate': 38.5, 'purchaseRate': 37.9}, {'baseCurrency': 'UAH', 'currency': 'UZS', 'saleRateNB': 0.0030243, 'purchaseRateNB': 0.0030243}, {'baseCurrency': 'UAH', 'currency': 'XAU', 'saleRateNB': 76207.32, 'purchaseRateNB': 76207.32}]}
 # for key, valume in result.items():
@@ -34,25 +38,28 @@ result = {'date': '19.02.2024', 'bank': 'PB', 'baseCurrency': 980, 'baseCurrency
 # print(tabulate(fdf, headers="keys"))
 # f = {"one": ["1"], "2": "two", "3": ["three"], "4": "2", "5": "five"}
 # print(tabulate(f, headers="keys"))
-
+# for currens in asyncio.run(main(sys.argv[1])):
 async def main():
-    async for currens in asyncio.run(main(sys.argv[1])):
-            
-            for key, valume in result.items():
-                if isinstance(valume, list):
-                    for val in valume:
-                        if isinstance(val, dict):
-                            for valum in val.values():
-                                if current==valum:
-                                    valu = val # dict
-                                    for kk, vv in valu.items():
-                                        valu[kk]=await str(vv).split()
+    logging.debug(f"result: ")
+    for valume in result.values():
+        if isinstance(valume, list):
+            for val in valume:
+                if isinstance(val, dict):
+                    for valum in val.values():
+                        if currensy==valum:
+                            valu = val # dict
+                            for kk, vv in valu.items():
+                                valu[kk]= str(vv).split()
+            # print("---------------------")
+            # print(valu)
+            # print("+++++++++++++++++++++++++++++++")
+                        
+
                             
-    
-                                
-                    head = ["baseCurrency", "currency", "sale", "purchase"]
-                    data = tabulate(valu, headers=head, tablefmt="grid")
-                    print(data)
+                            head = ["baseCurrency", "currency", "sale", "purchase"]
+                            data = tabulate(valu, headers=head, tablefmt="grid")
+                            
+                            return data
         # if currens == currensy:
         #     return currens
         # else:
